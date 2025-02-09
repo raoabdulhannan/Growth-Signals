7bd0d494c821e200e69914d4e7ee8a10dcee337b
AnanyaHN27 <ananya.harinarain@gmail.com>
2025-02-09 14:35:38 -0500

    Adding comments and TODOs

--- Growth-Signals/growth_signals/constants.py
+++ Growth-Signals/growth_signals/constants.py
@@ -1,6 +1,6 @@
 INPUT_DIM = 768
-HIDDEN_DIM = 128
+HIDDEN_DIM = 128  # TODO: optimize this with hyperparameter tuning
 BATCH_SIZE = 32
 EPOCHS = 10
 LR = 0.001
-EMBEDDING_DIM = 768  # Cohere
+EMBEDDING_DIM = 768  # Cohere embeddings are 768
